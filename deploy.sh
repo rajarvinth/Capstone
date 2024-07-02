dbf985aa3df62245104a572eef11a664c3781936
rajarvinth <163541064+rajarvinth@users.noreply.github.com>
2024-07-02 16:55:20 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,23 +1,25 @@
 #Fetching the current Git branch
-GIT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
-echo "Current Git Branch: $GIT_BRANCH"
-
+#!/bin/bash
 
 #Stop and remove existing containers
 docker-compose down
 
 #Docker login
-docker login -u $DOCKER_USERNAME --password $DOCKER_PASSWORD
+docker login -u arvinthraj --password dckr_pat_lb8Zhm3n6C4jiByNz-3wHUggD8s
 
 #Docker Prod step
+if [[ $GIT_BRANCH == "origin/dev" ]]; then
+    # Build your project
+    sh 'chmod +x build.sh'
+    sh './build.sh'
+    docker tag capdockerimg arvinthraj/prodcap1:v1
+    docker push arvinthraj/prodcap1:v1
 
-        ./build.sh
-        docker tag capdockerimg arvinthraj/prodcap1:v1
-        docker push arvinthraj/prodcap1:v1
-
-
-        ./build.sh
-        docker tag capdockerimg arvinthraj/devcap1:v1
-        docker push arvinthraj/devcap1:v1
-
+elif [[ $GIT_BRANCH == "origin/main" ]]; then
+    # Build your project
+    sh 'chmod +x build.sh'
+    sh './build.sh'
+    docker tag capdockerimg arvinthraj/devcap1:v1
+    docker push arvinthraj/devcap1:v1
+fi
 
