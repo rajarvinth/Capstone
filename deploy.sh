68f83cf3bf588c680fd523240cf66443a1913deb
rajarvinth <163541064+rajarvinth@users.noreply.github.com>
2024-07-02 16:25:50 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #Fetching the current Git branch
-BRANCH_NAME=${BRANCH_NAME:-$(git rev-parse --abbrev-ref HEAD)}
+BRANCH_NAME=$(git rev-parse --abbrev-ref HEAD)
 echo "Current Git Branch: ${BRANCH_NAME}"
 
 
