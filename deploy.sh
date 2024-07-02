d192b7d1009f7553940625d034e480dcf3f505e3
rajarvinth <163541064+rajarvinth@users.noreply.github.com>
2024-07-02 16:40:31 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -15,7 +15,7 @@ if [[ "$GIT_BRANCH" == "main" ]]; then
         docker tag capdockerimg arvinthraj/prodcap1:v1
         docker push arvinthraj/prodcap1:v1
 
-elif [[ "$GIT_BRANCH" == "origin/dev" ]]; then
+elif [[ "$GIT_BRANCH" == "dev" ]]; then
         ./build.sh
         docker tag capdockerimg arvinthraj/devcap1:v1
         docker push arvinthraj/devcap1:v1
