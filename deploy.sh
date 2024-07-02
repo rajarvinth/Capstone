48f80bb420e7fc9009061f377606407ac733670c
rajarvinth <163541064+rajarvinth@users.noreply.github.com>
2024-07-02 16:37:36 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #Fetching the current Git branch
-BRANCH_NAME=$(git rev-parse --abbrev-ref HEAD)
+GIT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
 echo "Current Git Branch: ${BRANCH_NAME}"
 
 
@@ -10,12 +10,12 @@ docker-compose down
 docker login -u $DOCKER_USERNAME --password $DOCKER_PASSWORD
 
 #Docker Prod step
-if [[ "$BRANCH_NAME" == "main" ]]; then
+if [[ "$GIT_BRANCH" == "main" ]]; then
         ./build.sh
         docker tag capdockerimg arvinthraj/prodcap1:v1
         docker push arvinthraj/prodcap1:v1
 
-elif [[ "$BRANCH_NAME" == "origin/dev" ]]; then
+elif [[ "$GIT_BRANCH" == "origin/dev" ]]; then
         ./build.sh
         docker tag capdockerimg arvinthraj/devcap1:v1
         docker push arvinthraj/devcap1:v1
