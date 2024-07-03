db3c9704b6c27d96615e04bd39ae8b305f72840c
rajarvinth <163541064+rajarvinth@users.noreply.github.com>
2024-07-03 11:24:53 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -5,11 +5,22 @@
 docker-compose down
 
 #Docker login
-docker login -u arvinthraj --password dckr_pat_Mb9SYa6cILLou8FmsKcn4GUPgTA
+echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 
 #Docker Prod step
+if [[ $GIT_BRANCH == "origin/dev" ]]; then
+    # Build your project
+    sh 'chmod +x build.sh'
+    sh './build.sh'
+    docker tag capdockerimg arvinthraj/prodcap1:v1
+    docker push arvinthraj/prodcap1:v1
+
+elif [[ $GIT_BRANCH == "origin/main" ]]; then
     # Build your project
     sh 'chmod +x build.sh'
     sh './build.sh'
     docker tag capdockerimg arvinthraj/devcap1:v1
     docker push arvinthraj/devcap1:v1
+fi
+
+
